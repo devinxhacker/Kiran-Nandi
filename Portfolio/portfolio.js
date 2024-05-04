efc5176c5658604c8f622859dcfb5ab9ed43c580
kirannandi896 <kirannandi896@gmail.com>
2024-05-04 17:39:56 +0530

    head blur

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,24 +1,50 @@
+function openNav() {
+  document.getElementById("navo").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("navo").style.height = "0%";
+}
 
 function active() {
-    const navLinks = document.querySelectorAll('.overlay-content a');
-    navLinks.forEach(link => {
-      link.addEventListener('click', function() {
-        navLinks.forEach(link => {
-          link.classList.remove('active');
-        });
-        this.classList.add('active');
+  const navLinks = document.querySelectorAll('.overlay-content a');
+  navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
       });
+      this.classList.add('active');
     });
-    // document.getElementById("nav").style.height = "0%";
+  });
+  document.getElementById("navo").style.height = "0%";
 }
-$(window).bind('keydown', 'ctrl+s', function () {
-  $('#save').click(); return false;
-});
+
+function activly(){
+  const navLinks = document.querySelectorAll('.nav-content a');
+  navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+      this.classList.add('active');
+    });
+  });
+  active();
+}
+
+// $(window).bind('keydown', 'ctrl+s', function () {
+//   $('#save').click(); return false;
+// });
 DisableDevtool({
   ondevtoolopen: (type) => {
     if(DisableDevtool.isRunning){console.log("Copyright restrictions are in effect."); window.location.href="error.html";}
   },
 });
+
+
+
+
+
 document.onkeydown = (e) => {
   if ( 
     event.keyCode === 123 ||
@@ -28,8 +54,7 @@ document.onkeydown = (e) => {
     ctrlShiftKey(e, "J") ||
     ctrlShiftKey(e, "C") ||
     (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
-  )
-    return !1;
+  ) return !1;
 };
 
 
