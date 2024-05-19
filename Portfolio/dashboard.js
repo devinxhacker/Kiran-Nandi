1394495d3a9b079178c2635b1e94b4fea5343bf5
kirannandi896 <kirannandi896@gmail.com>
2024-05-19 15:34:13 +0530

    Update

--- Portfolio/dashboard.js
+++ Portfolio/dashboard.js
@@ -8,6 +8,99 @@ window.onload = function () {
     }, 3000);
     play();
     body.style.overflow = "auto";
+    var audio = document.getElementById("audio");
+    var icon = document.getElementById("audio-toggler");
+    audio.addEventListener("pause",function(){
+        if (audio.paused) {
+            icon.classList.remove("fa-pause");
+            icon.classList.add("fa-play");
+        } else {
+            icon.classList.remove("fa-play");
+            icon.classList.add("fa-pause");
+        }
+    })
+
+    audio.addEventListener("play",function(){
+        if (audio.paused) {
+            icon.classList.remove("fa-pause");
+            icon.classList.add("fa-play");
+        } else {
+            icon.classList.remove("fa-play");
+            icon.classList.add("fa-pause");
+        }
+    })
+
+}
+
+
+function toggleAudio(){
+    var audio = document.getElementById("audio");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+
+    var icon = document.getElementById("audio-toggler");
+    if (icon.classList.contains("fa-pause")) {
+        icon.classList.remove("fa-pause");
+        icon.classList.add("fa-play");
+    } else {
+        icon.classList.remove("fa-play");
+        icon.classList.add("fa-pause");
+    }
+}
+
+function deblur() {
+    var canvas = document.getElementById("canvas2");
+    if (canvas.style.display == "none") {
+        canvas.style.display = "block";
+    }
+    else{
+        canvas.style.display = "none";
+    }
+
+    var icon = document.getElementById("deblur-toggler");
+    if (icon.classList.contains("fa-eye")) {
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
+
+
+function display_container(){
+    var container = document.getElementsByClassName("container")[0];
+    if (container.style.display == "grid" ) {
+        container.style.display = "none";
+    }
+    else{
+        container.style.display = "grid";
+    }
+
+    var icon = document.getElementById("container_toggler");
+    if (icon.classList.contains("fa-angle-down")) {
+        icon.classList.remove("fa-angle-down");
+        icon.classList.add("fa-angle-up");
+    } else {
+        icon.classList.remove("fa-angle-up");
+        icon.classList.add("fa-angle-down");
+    }
+}
+
+function random_music(audio) {
+    audio.pause();
+    audio.currentTime = 0;
+    var rand = Math.floor(Math.random() * 3) + 1;
+    audio.src = "musics/audio" + 1 + ".mp3";
+    audio.play();
+    // It should play other audio when the first one ends
+    audio.addEventListener("ended", function () {
+        random_music(audio);
+    })
+    // audio.loop = true;
 }
 
 function play() {
@@ -23,8 +116,7 @@ function play() {
 
 function aplay() {
     var audio = document.getElementById("audio");
-    audio.play();
-    audio.loop = true;
+    random_music(audio);
     var context = new AudioContext();
     var src = context.createMediaElementSource(audio);
     var analyser = context.createAnalyser();
@@ -47,9 +139,13 @@ function aplay() {
     var WIDTH = canvas.width;
     var HEIGHT = canvas.height;
 
-    var barWidth = (WIDTH / bufferLength) * 2.5;
+    console.log(WIDTH, HEIGHT);
+
+    var barWidth = (WIDTH / bufferLength) * 2;
     var barHeight;
     var x = 0;
+
+    console.log(barWidth, barHeight, x);
 
     function renderFrame() {
         requestAnimationFrame(renderFrame);
@@ -64,9 +160,9 @@ function aplay() {
         for (var i = 0; i < bufferLength; i++) {
             barHeight = dataArray[i];
 
-            var r = barHeight + (25 * (i / bufferLength));
+            var r = barHeight + (10 * (i / bufferLength));
             var g = 250 * (i / bufferLength);
-            var b = 50;
+            var b = 250;
 
             ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
             ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
@@ -75,9 +171,8 @@ function aplay() {
         }
     }
 
+
     audio.play();
     renderFrame();
 };
-
-
 
